e6f52693e430785040375a6cd64ec0caa38f6b07
Dubem Ikechukwu <mightyeaglecorp@gmail.com>
2022-02-13 21:33:56 +0100

    made change'

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import morgan from "morgan";
 import { ConnectDatabase } from "./database.js";
 
 dotenv.config();
@@ -11,6 +12,10 @@ ConnectDatabase();
 const PORT = process.env.PORT || 3112;
 
 app.use(express.static("public"));
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 app.use(cors());
 app.get("/", (req, res) => {
   res.json({ message: "Server is alive" });
