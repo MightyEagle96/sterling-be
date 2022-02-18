987d3c55f90a4971454d1951b516ca56502e85df
Dubem Ikechukwu <mightyeaglecorp@gmail.com>
2022-02-19 00:48:10 +0100

    made changes

--- services.js
+++ services.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
 export const originUrl =
   process.env.NODE_ENV === 'production'
-    ? 'https://sterling-47.web.app/'
+    ? 'https://sterling-47.web.app'
     : 'http://localhost:3000';
